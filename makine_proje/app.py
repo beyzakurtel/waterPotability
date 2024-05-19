31cc789abdc29b6cd2bda28260f5c02f288119b1
beyzakurtel <030121024@std.izu.edu.tr>
2024-05-19 21:14:22 +0300

    commit2

--- makine_proje/app.py
+++ makine_proje/app.py
@@ -1,3 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score, GridSearchCV, learning_curve
+from sklearn.feature_selection import RFE
 from sklearn.feature_selection import RFE
 import pandas as pd
 import numpy as np
